4cb58710649407176ade16925e5da5cf9629e24c
StephenKhytze <stephen092madula@gmail.com>
2026-02-11 20:39:15 +0800

    tablet view finalized

--- dist/js/burgir.js
+++ dist/js/burgir.js
@@ -9,28 +9,39 @@ burgirBtn.addEventListener("click", () => {
 const menuNav = document.querySelectorAll(".tab__nav__link");
 const menuTab = document.querySelector(".menu__content")
 const menuContent = document.querySelectorAll(".menu__content__item");
+const sliderNav = document.querySelectorAll(".slider__nav__container")
 
 menuNav.forEach((menu) => {
     menu.addEventListener('click', () => {
         removeActiveMenu();
+        removeActiveNav();
         menu.classList.add('active');
         menuTab.style.opacity = 0;
         menuTab.style.transform = "translateX(-30rem)"
         setTimeout(() => {
             menuTab.style.transform = "translateX(30rem)"
         }, 100);
-
         setTimeout(() => {
             const activeContent = document.querySelectorAll(`#${menu.id}-content`);
             removeActiveContent();
             activeContent.forEach((content) => {
-            content.classList.add('active');
-        });
+                content.classList.add('active');
+            });
             menuTab.style.transform = "translateX(0)"
             menuTab.style.opacity = 1;
         }, 250);
+        const currSliderNav = document.querySelector(`#${menu.id}-content-nav`)
+        if (currSliderNav) {
+            currSliderNav.classList.add("active");
+        }
     });
 });
+
+function removeActiveNav() {
+    sliderNav.forEach((nav) => {
+        nav.classList.remove("active")
+    })
+}
 
 function removeActiveMenu() {
     menuNav.forEach((menu) => {
